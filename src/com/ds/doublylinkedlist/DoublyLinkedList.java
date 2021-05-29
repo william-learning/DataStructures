893aa9f164edba6b86fce9661ab73a80da302b0e
williamsoftwarecode <williamsoftwarecode@gmail.com>
2021-05-29 18:27:30 +0800

    Implemented push() for DoublyLinkedList

--- src/com/ds/doublylinkedlist/DoublyLinkedList.java
+++ src/com/ds/doublylinkedlist/DoublyLinkedList.java
@@ -10,4 +10,48 @@ public class DoublyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+
+    // Create a new node with the new value
+    // Edge case: If the length is equal to 0, set the head and tail to be the new node
+    // Otherwise, set the next property of the tail to be the new node
+    // Set the prev property of the new node to the tail
+    // Set the tail to be the new node
+    // Increment the length
+    // Return the list
+    public <T> DoublyLinkedList push(final T val) {
+        final Node node = new Node<>(val);
+
+        if (this.length == 0) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.tail.setNext(node);
+            node.setPrev(this.tail);
+            this.tail = node;
+        }
+
+        this.length++;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return this.head != null ? this.head.printAllNodes() : "";
+    }
+
+
+
+    // Accessor methods
+
+    Node getHead() {
+        return this.head;
+    }
+
+    Node getTail() {
+        return this.tail;
+    }
+
+    int getLength() {
+        return this.length;
+    }
 }
--- test/com/ds/doublylinkedlist/DoublyLinkedListTest.java
+++ test/com/ds/doublylinkedlist/DoublyLinkedListTest.java
@@ -1,4 +1,38 @@
 package com.ds.doublylinkedlist;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DoublyLinkedListTest {
+
+    DoublyLinkedList list;
+
+    @BeforeEach
+    void setup() {
+        this.list = new DoublyLinkedList();
+        this.list.push("Hi");
+        this.list.push("There");
+    }
+
+    @Test
+    void testPush() {
+        this.list.push("DoublyLinkedList");
+        assertEquals(this.list.toString(), "HiThereDoublyLinkedList");
+
+        assertEquals(this.list.getTail().getPrev().toString(), "There");
+        assertEquals(this.list.getTail().getPrev().getNext().toString(), "DoublyLinkedList");
+
+        assertEquals(this.list.getHead().toString(), "Hi");
+        assertEquals(this.list.getTail().toString(), "DoublyLinkedList");
+        assertEquals(this.list.getLength(), 3);
+
+        // Test push() for an empty list
+        this.list = new DoublyLinkedList();
+        this.list.push("DoublyLinkedList");
+        assertEquals(this.list.toString(), "DoublyLinkedList");
+        assertEquals(this.list.getHead(), this.list.getTail());
+        assertEquals(this.list.getLength(), 1);
+    }
 }
